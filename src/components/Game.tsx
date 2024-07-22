f363f21f2b04602504486fe7b99985046458d4e4
Volky <volkyeth@pm.me>
2024-07-22 13:47:11 -0300

    Change pay tab icon

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -10,6 +10,7 @@ import { PayTab } from "@/components/tabs/Pay/PayTab";
 import { ValuationsTab } from "@/components/tabs/Valuations/ValuationsTab";
 import { WalletTab } from "@/components/tabs/Wallet/WalletTab";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { cn } from "@/lib/cn";
 import { useNotifications } from "@/lib/game/useNotifications";
 import useWakeLock from "@/lib/game/useWakeLock";
 import { useAtom, useAtomValue } from "jotai";
@@ -17,9 +18,8 @@ import {
   AreaChartIcon,
   HandHeartIcon,
   HomeIcon,
+  SendIcon,
   Settings2Icon,
-  StoreIcon,
-  UsersRoundIcon,
 } from "lucide-react";
 
 export const Game = () => {
@@ -58,12 +58,12 @@ export const Game = () => {
       <TabsList className="w-full shrink-0 rounded-none bg-muted m-0 p-2 justify-evenly gap-2 h-16 border-t overflow-clip">
         {Object.entries(
           currentPlayer.isDealer
-            ? { wallet: HomeIcon, pay: UsersRoundIcon, market: AreaChartIcon }
+            ? { wallet: HomeIcon, pay: SendIcon, market: AreaChartIcon }
             : {
                 wallet: HomeIcon,
                 valuations: Settings2Icon,
+                pay: SendIcon,
                 causes: HandHeartIcon,
-                pay: StoreIcon,
                 market: AreaChartIcon,
               },
         ).map(([key, Icon]) => (
@@ -72,7 +72,7 @@ export const Game = () => {
             value={key}
             className="flex-grow flex gap-2 h-full data-[state=active]:bg-foreground/5 data-[state=active]:shadow-inner"
           >
-            <Icon className="inline size-6" />
+            <Icon className={cn("inline size-6", key === "pay" && "size-7")} />
           </TabsTrigger>
         ))}
       </TabsList>
